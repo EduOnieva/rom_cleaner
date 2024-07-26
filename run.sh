81e0a3c19a08be6a93d3c62ff10e9f8a7ab05e06
Eduardo Onieva <eduonieva@gmail.com>
2024-07-26 11:17:48 +0200

    Added --rm to the docker run

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ echo "Building the docker image..."
 docker build -t rom_cleaner:latest .
 
 echo "Running the docker image..."
-docker run -it rom_cleaner --regions=$1 --url=$2
+docker run -it --name rom_cleaner --rm rom_cleaner  --regions=$1 --url=$2
 
 echo "Done!"
